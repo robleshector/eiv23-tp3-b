ab1ab9c6b37e74dfdd240329c1329bf04e2ff396
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2023-04-27 15:45:06 -0300

    Pulsador en B9, cambio en expr. BSRR

--- src/main.c
+++ src/main.c
@@ -4,22 +4,22 @@
 enum {LUZ_ON=0,LUZ_OFF=1,PULSADOR_ACTIVO=0,PULSADOR_NORMAL=1};
 
 int main(void){
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN;
-    // A15 pulsador, con resistencia pull hacia el nivel normal
-    GPIOA->BSRR = 1 << ((PULSADOR_NORMAL)? 15:15+16);
-    GPIOA->CRH =   (GPIOA->CRH & ~(GPIO_CRH_MODE15 | GPIO_CRH_CNF15))
-                 | GPIO_CRH_CNF15_1;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
+    // B9 pulsador, con resistencia pull hacia el nivel normal
+    GPIOB->BSRR = 1 << (9+((PULSADOR_NORMAL)? 0:16));
+    GPIOB->CRH =   (GPIOB->CRH & ~(GPIO_CRH_MODE9 | GPIO_CRH_CNF9))
+                 | GPIO_CRH_CNF9_1;
     // C13 luz, inicialmente apagada
-    GPIOC->BSRR = 1 << ((LUZ_OFF)? 13:13+16);
+    GPIOC->BSRR = 1 << (13+((LUZ_OFF)? 0:16));
     GPIOC->CRH =   (GPIOC->CRH & ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13))
                  | GPIO_CRH_MODE13_1;
     for(;;){
-        while((GPIOA->IDR & (1<<15)) != (PULSADOR_ACTIVO << 15));
-        GPIOC->BSRR = 1 << ((LUZ_ON)? 13:13+16);
+        while((GPIOB->IDR & (1<<9)) != (PULSADOR_ACTIVO << 9));
+        GPIOC->BSRR = 1 << (13+((LUZ_ON)? 0:16));
         for(int j=0;j<60000;++j){
             for(int volatile i=0;i<(8000000/(1000*13));++i);
         }
-        GPIOC->BSRR = 1 << ((LUZ_OFF)? 13:13+16);
+        GPIOC->BSRR = 1 << (13+((LUZ_OFF)? 0:16));
     }
     return 0;
 }

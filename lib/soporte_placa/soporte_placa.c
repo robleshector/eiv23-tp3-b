cec3d95b95dcc42284d1bd1b3d63008de9ac7921
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2023-05-05 11:42:44 -0300

    Macros de utilidad

--- lib/soporte_placa/soporte_placa.c
+++ lib/soporte_placa/soporte_placa.c
@@ -13,8 +13,16 @@ static Pin const pines[SP_HPIN_LIMITE] = {
     [SP_PC13]={.puerto=GPIOC,.nrPin=13}
 };
 
+/**
+ * @brief Obtiene el objeto pin a partir de su handle
+ * 
+ */
 #define PIN_DE_HANDLE(hpin) (pines[hpin])
-#define GPIO_A_ENR_BIT(gpio) ((gpio >> 10) & 0x7)
+/**
+ * @brief Calcula la posición en del bit de habilitación
+ * del puerto en APB2_ENR a partir de su dirección en memoria.
+ */
+#define GPIO_A_ENR_BIT(gpio) (((uint32_t)(gpio) >> 10) & 0xF)
 // ... continúa implementación
 
 void SP_Pin_setModo(SP_HPin hpin,SP_Pin_Modo modo){
